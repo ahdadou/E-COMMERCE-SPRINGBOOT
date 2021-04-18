12b1d9cc35cf2e7bd2b055015cab6fdf0e5c7029
ahdadou <ibra.ahdadou@gmail.com>
2021-04-18 20:47:44 +0000

    create : Order class && OrderDetails class

--- src/main/java/com/shop/demo/entity/Orders.java
+++ src/main/java/com/shop/demo/entity/Orders.java
@@ -1,5 +1,40 @@
 package com.shop.demo.entity;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_order")
 public class Orders {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long order_id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	private String name;
+	private String phone;
+	private String adress;
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+	private String city;
+	private int zipCode;
+	private Date date_order;
+	
 
 }
--- src/main/java/com/shop/demo/entity/OrdersDetails.java
+++ src/main/java/com/shop/demo/entity/OrdersDetails.java
@@ -1,5 +1,37 @@
 package com.shop.demo.entity;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "order_details")
 public class OrdersDetails {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long order_detail_id;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Orders order;
+	
+	private int quantity;
+	
+	private double price;
 
 }
